a83b9fc49c79ddaf29df21759472037ff2927d94
carsago <ohseo8813@gmail.com>
2025-01-05 20:59:27 +0900

    feat: tsconfigPaths 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      'assets': path.resolve(__dirname, 'src/assets'),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 });
