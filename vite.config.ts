6c8e2c6b0349a66a4e70dd5bc9096e288fa10e42
carsago <ohseo8813@gmail.com>
2025-01-05 23:01:02 +0900

    feat submoudle 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dotenv from 'dotenv';
+import path from 'path';
+
+const mode = process.env.NODE_ENV || 'development';
+const envFile = path.resolve(__dirname, `front-submodule/.env.${mode}`);
+dotenv.config({ path: envFile });
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  define: {
+    'process.env': process.env,
+  },
 });
