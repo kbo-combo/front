2da11a80cfa7cfacbf50dfec8fa8e8571e7ef714
carsago <ohseo8813@gmail.com>
2025-01-05 16:52:25 +0900

    feat: theme 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
 import {GlobalStyle} from "./style/Global.style.ts";
+import {ThemeProvider} from "styled-components";
+import theme from "./style/theme.style.ts";
 
 function App() {
 
   return (
     <>
       <GlobalStyle />
+      <ThemeProvider theme={theme}>
       <div>안녕하세요 여러분</div>
+      </ThemeProvider>
     </>
   )
 }
