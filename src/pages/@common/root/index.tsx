ddca221856a66ad07de16f9c61f46d0a36bab736
carsago <ohseo8813@gmail.com>
2025-01-05 18:53:34 +0900

    feat: 특정 페이지에서 NavBar 가리는 기능 추가

--- src/pages/@common/root/index.tsx
+++ src/pages/@common/root/index.tsx
@@ -1,14 +1,22 @@
 import {PageArea, Wrapper} from "./RootTemplate.style.ts";
+import {matchRoutes, Outlet, useLocation} from "react-router-dom";
+import {URL_PATH} from "../../../constant";
 import NavBar from "../../../components/@common/navbar/NavBar.tsx";
-import {Outlet} from "react-router-dom";
+
+const NO_NAVIGATION_BAR_URLS = [
+  URL_PATH.login,
+].map((path) => ({ path }));
 
 const RootTemplate = () => {
+
+  const { pathname } = useLocation();
+  const showNavBar = matchRoutes(NO_NAVIGATION_BAR_URLS, pathname) === null;
 
   return (
       <Wrapper>
         <PageArea>
           <Outlet />
-          <NavBar/>
+          {showNavBar && <NavBar />}
         </PageArea>
       </Wrapper>
   );
