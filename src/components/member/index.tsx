5b4539fab759a29c1c698704e60fbd22ae9cc477
carsago <ohseo8813@gmail.com>
2025-01-06 23:19:21 +0900

    feat: 멤버 수정 Link 추가

--- src/components/member/index.tsx
+++ src/components/member/index.tsx
@@ -7,6 +7,8 @@ import {
 } from "@components/member/member.style.tsx";
 import ContentHeader from "@components/@common/contentHeader";
 import {FiEdit3} from "react-icons/fi";
+import {Link} from "react-router-dom";
+import {URL_PATH} from "@/constant";
 
 
 const MemberPage = () => {
@@ -16,9 +18,11 @@ const MemberPage = () => {
     <ProfileWrapper>
       <ProfileImage src="/santa-sunnam.png" alt="프로필 이미지"/>
       <Nickname>산타 선남 이름을 좀 길게 해보자</Nickname>
+      <Link to={`${URL_PATH.member}/modify`}>
       <EditButton>
         <FiEdit3 size={16} color='white'/>
       </EditButton>
+      </Link>
     </ProfileWrapper>
   </Wrapper>
 
