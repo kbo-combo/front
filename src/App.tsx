186722e831adb4f21ceffc9c25ca8af7f002f62a
carsago <ohseo8813@gmail.com>
2025-01-06 15:27:53 +0900

    feat: robots.txt 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,18 @@ import theme from "@style/theme.style";
 import {RouterProvider} from "react-router-dom";
 import router from "./router.tsx";
 import SvgSpriteMap from "@components/@common/icons/SvgSpriteMap";
+import {Helmet} from "react-helmet-async";
 
 function App() {
 
   return (
       <>
         <GlobalStyle/>
+        <Helmet>
+          {process.env.NODE_ENV !== 'production' && (
+              <meta name="robots" content="noindex, nofollow" />
+          )}
+        </Helmet>
         <ThemeProvider theme={theme}>
           <SvgSpriteMap/>
           <RouterProvider router={router}/>
