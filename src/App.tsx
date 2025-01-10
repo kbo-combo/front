31e46908f42e780823c7570ce59aa9dfb1df1a9a
carsago <ohseo8813@gmail.com>
2025-01-10 09:19:29 +0900

    feat: @tanstack/react-query 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,29 @@ import {RouterProvider} from "react-router-dom";
 import router from "./router.tsx";
 import SvgSpriteMap from "@components/@common/icons/SvgSpriteMap";
 import {Helmet, HelmetProvider} from "react-helmet-async";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 
 function App() {
 
   return (
       <>
-        <HelmetProvider>
-        <GlobalStyle/>
-        <Helmet>
-          {process.env.NODE_ENV !== 'production' && (
-              <meta name="robots" content="noindex, nofollow" />
-          )}
-        </Helmet>
-        <ThemeProvider theme={theme}>
-          <SvgSpriteMap/>
-          <RouterProvider router={router}/>
-        </ThemeProvider>
-        </HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <HelmetProvider>
+            <GlobalStyle/>
+            <Helmet>
+              {process.env.NODE_ENV !== 'production' && (
+                  <meta name="robots" content="noindex, nofollow"/>
+              )}
+            </Helmet>
+            <ThemeProvider theme={theme}>
+              <SvgSpriteMap/>
+              <RouterProvider router={router}/>
+            </ThemeProvider>
+          </HelmetProvider>
+        </QueryClientProvider>
       </>
   )
 }
