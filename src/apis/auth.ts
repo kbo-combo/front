858591ed497aab9a25ed1995a2df3ac1606bd793
carsago <ohseo8813@gmail.com>
2025-01-11 14:47:17 +0900

    refactor: redirect 하도록 수정

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -4,7 +4,12 @@ export interface LoginRequest {
   socialProvider: string;
 }
 
-export const getLoginPage = async (loginRequest: LoginRequest, redirectUri: string): Promise<string> => {
-  const response = await client.get<string>(`/oauth/${loginRequest.socialProvider}?redirectUri=${encodeURIComponent(redirectUri)}`);
-  return response.data
+export const getLoginPage = async (
+    loginRequest: LoginRequest,
+    redirectUri: string
+): Promise<{ redirectUri: string }> => {
+  const response = await client.get<{ redirectUri: string }>(
+      `/oauth/${loginRequest.socialProvider}?redirectUri=${encodeURIComponent(redirectUri)}`
+  );
+  return response.data;
 };
--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,15 +1,17 @@
-import {useMutation} from '@tanstack/react-query';
-import {getLoginPage, LoginRequest} from "@apis/auth.ts";
-import {URL_PATH} from "@/constant";
-
+import { useMutation } from '@tanstack/react-query';
+import { getLoginPage, LoginRequest } from "@apis/auth.ts";
+import { URL_PATH } from "@/constant";
 
 const useLogin = () => {
-
-  const redirectUri = `${window.location.origin}${URL_PATH.main}`;
+  const redirectUri = `${window.location.origin}${URL_PATH.login}`;
 
   const loginMutation = useMutation({
-    mutationFn:({request}: { request: LoginRequest}) =>
+    mutationFn: ({ request }: { request: LoginRequest }) =>
         getLoginPage(request, redirectUri),
+    onSuccess: (data: { redirectUri: string }) => {
+      // 서버에서 반환된 redirectUri로 브라우저 리디렉션
+      window.location.href = data.redirectUri;
+    },
     onError: () => {
       alert('로그인 요청에 실패했습니다. 다시 시도해주세요.');
     },
@@ -19,7 +21,7 @@ const useLogin = () => {
     loginMutation.mutate({ request });
   };
 
-  return {login};
+  return { login };
 };
 
 export default useLogin;
