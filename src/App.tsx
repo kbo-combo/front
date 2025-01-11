66bf6cc0cd37c884a8631ab1687c3f09e2d5b75c
carsago <ohseo8813@gmail.com>
2025-01-11 17:36:04 +0900

    refactor: 라우터 패키지 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import {GlobalStyle} from '@style/Global.style';
 import {ThemeProvider} from "styled-components";
 import theme from "@style/theme.style";
 import {RouterProvider} from "react-router-dom";
-import router from "./router.tsx";
 import SvgSpriteMap from "@components/@common/icons/SvgSpriteMap";
 import {Helmet, HelmetProvider} from "react-helmet-async";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import router from "@/router/router.tsx";
 
 const queryClient = new QueryClient();
 
