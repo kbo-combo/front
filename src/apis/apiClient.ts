83d3f3b9dc0f2b236f976589d76f84700d355768
carsago <ohseo8813@gmail.com>
2025-01-13 22:45:58 +0900

    refactor: allowCredentail 제거

--- src/apis/apiClient.ts
+++ src/apis/apiClient.ts
@@ -4,7 +4,6 @@ const BASE_URL  = import.meta.env.VITE_SERVER_URL
 
 const defaultConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
-  withCredentials: true
 };
 
 export const client = axios.create(defaultConfig);
