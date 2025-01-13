23071e12974d210db2c819300affb06f65c3d10c
carsago <ohseo8813@gmail.com>
2025-01-13 23:26:10 +0900

    refactor: svg 못가져오는 문제 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,18 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import dotenv from 'dotenv';
 import path from 'path';
-
 const mode = process.env.NODE_ENV || 'development';
 const envFile = path.resolve(__dirname, `front-submodule/.env.${mode}`);
 dotenv.config({ path: envFile });
+import svgr from 'vite-plugin-svgr';
+
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
   define: {
     'process.env': process.env,
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve('src') }],
   },
 });
