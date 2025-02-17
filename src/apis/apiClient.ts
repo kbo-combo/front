5802f8176aff6e83023ac312119a105b9a70cd3e
carsago <ohseo8813@gmail.com>
2025-02-17 12:24:03 +0900

    refactor: 401 에러시 로그인 하도록 수정

--- src/apis/apiClient.ts
+++ src/apis/apiClient.ts
@@ -1,10 +1,30 @@
-import axios, {AxiosRequestConfig} from "axios";
+import axios, { AxiosRequestConfig } from "axios";
+import { URL_PATH } from "@/constant";
 
 const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
 const defaultConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
-  withCredentials: true
+  withCredentials: true,
 };
 
 export const client = axios.create(defaultConfig);
+
+let isInterceptorSet = false;
+
+export const setupInterceptors = () => {
+  if (isInterceptorSet) return;
+  isInterceptorSet = true;
+
+  client.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response?.status === 401) {
+          window.location.href = `${URL_PATH.login}`;
+        }
+        return Promise.reject(error);
+      }
+  );
+};
+
+setupInterceptors();
