62f34ed5ae52fb2cc15c32518749049f5003ebb9
carsago <ohseo8813@gmail.com>
2025-02-17 12:11:38 +0900

    refactor: 로그인해서 선택 콤보 보이도록 수정

--- src/pages/@common/home/index.tsx
+++ src/pages/@common/home/index.tsx
@@ -3,7 +3,6 @@ import GameList from "@components/game/GameList.tsx";
 import GameSchedule from "@components/game/GameSchedule.tsx";
 import SelectedCombo from "@components/combo/SelectedCombo.tsx";
 import {useCheckLogin} from "@/hooks/login.ts";
-import SelectedComboError from "@components/combo/SelectedComboError.tsx";
 
 
 const Home = () => {
@@ -14,7 +13,7 @@ const Home = () => {
       <Main>
         <MainMessage>하루, 한타</MainMessage>
           <GameSchedule/>
-          {isLoggedIn ? <SelectedCombo /> : <SelectedComboError message="로그인 이후 콤보 선택이 가능합니다" />}
+          {isLoggedIn ? <SelectedCombo /> : <div></div>}
           <GameList/>
       </Main>
   );
