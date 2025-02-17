6f0c8ebc227559ff0089516719f5e15d3bb55aa6
carsago <ohseo8813@gmail.com>
2025-02-17 21:13:47 +0900

    refactor: 재로그인시 세션 삭제

--- src/apis/apiClient.ts
+++ src/apis/apiClient.ts
@@ -1,5 +1,6 @@
 import axios, {AxiosError, AxiosRequestConfig} from "axios";
 import { URL_PATH } from "@/constant";
+import {deleteSession} from "@/utils/LoginStorage.ts";
 
 const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
@@ -20,7 +21,8 @@ export const setupInterceptors = () => {
       (response) => response,
       (error: AxiosError<{ msg?: string }>) => {
         if (error.response?.status === 401) {
-          window.location.href = `${URL_PATH.login}?redirect=${encodeURIComponent(window.location.pathname)}`;
+          deleteSession()
+          window.location.href = `${URL_PATH.login}`;
         }
 
         return Promise.reject(new Error(error.response?.data.msg || "알 수 없는 오류가 발생했습니다."));
--- src/utils/LoginStorage.ts
+++ src/utils/LoginStorage.ts
@@ -8,6 +8,11 @@ export const setSession = () => {
   localStorage.setItem(StorageKeys.LOGIN_KEY, "anyValue");
 };
 
+export const deleteSession = () => {
+  localStorage.removeItem(StorageKeys.LOGIN_KEY);
+};
+
+
 export const getSession = (): string | null => {
   return localStorage.getItem(StorageKeys.LOGIN_KEY);
 };
