11783741b088e47b1752b4dd8cead820bf669e30
carsago <ohseo8813@gmail.com>
2025-03-06 02:21:08 +0900

    feat: 닉네임 max 길이 추가

--- src/pages/@common/member/MemberEditPage.tsx
+++ src/pages/@common/member/MemberEditPage.tsx
@@ -6,7 +6,10 @@ import {URL_PATH} from "@/constant";
 import Loading from "@pages/@common/common/Loading.tsx";
 import {PageWrapper} from "@components/@common/wrapper/pageWrapper.style.ts";
 import {ProfileWrapper} from "@pages/@common/member/member.style.ts";
-import {NicknameInput, SaveButton} from "@pages/@common/member/member-edit.style.ts";
+import {CharCount, NicknameInput, SaveButton} from "@pages/@common/member/member-edit.style.ts";
+
+
+const MAX_NICKNAME_LENGTH = 30;
 
 const MemberPage = () => {
   const { data, isLoading, error } = useMemberDetail();
@@ -15,16 +18,13 @@ const MemberPage = () => {
 
   const [nickname, setNickname] = useState("");
 
-
   useEffect(() => {
     if (data?.nickname) {
       setNickname(data.nickname);
     }
   }, [data]);
 
-
   const isChanged = nickname.trim() !== data?.nickname;
-
 
   const handleNicknameChange = () => {
     if (!isChanged) return;
@@ -35,6 +35,13 @@ const MemberPage = () => {
     });
   };
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newNickname = e.target.value;
+    if (newNickname.length <= MAX_NICKNAME_LENGTH) {
+      setNickname(newNickname);
+    }
+  };
+
   if (isLoading) return <Loading />;
   if (error || !data) return <div>데이터를 불러올 수 없습니다.</div>;
 
@@ -42,11 +49,13 @@ const MemberPage = () => {
       <PageWrapper>
         <ContentHeader title="마이페이지" />
         <ProfileWrapper>
-          <NicknameInput
-              type="text"
-              value={nickname}
-              onChange={(e) => setNickname(e.target.value)}
-          />
+            <NicknameInput
+                type="text"
+                value={nickname}
+                onChange={handleInputChange}
+                maxLength={MAX_NICKNAME_LENGTH}
+            />
+            <CharCount>{nickname.length} / {MAX_NICKNAME_LENGTH}</CharCount>
           <SaveButton onClick={handleNicknameChange} disabled={!isChanged}>
             닉네임 변경
           </SaveButton>
--- src/pages/@common/member/member-edit.style.ts
+++ src/pages/@common/member/member-edit.style.ts
@@ -3,9 +3,17 @@ import styled from "styled-components";
 export const NicknameInput = styled.input`
   width: 80%;
   padding: 1rem;
-  font: bold 1.5rem / 2.2rem "jua";
+  font: bold 1.3rem / 2.0rem "jua";
   border: 0.25rem solid ${({ theme: { color } }) => color.gray};
   border-radius: 1rem;
+`;
+
+export const CharCount = styled.span`
+  font-size: 1.2rem;
+  color: gray;
+  right: 1rem;
+  bottom: 0.2rem;
+  pointer-events: none; 
 `;
 
 export const SaveButton = styled.button<{ disabled?: boolean }>`
@@ -25,4 +33,3 @@ export const SaveButton = styled.button<{ disabled?: boolean }>`
     background: ${({ theme, disabled }) => (disabled ? theme.color.gray : theme.color.water)};
   }
 `;
-
