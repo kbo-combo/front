5d9463d3e14be682f83a4026b3ac69a8191fe1e0
carsago <ohseo8813@gmail.com>
2025-03-06 02:51:00 +0900

    feat: 로그인 만료시 토스트 추가

--- src/router/AuthRoute.tsx
+++ src/router/AuthRoute.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import {Navigate} from 'react-router-dom';
 import {URL_PATH} from "@/constant";
 import {useCheckLogin} from "@/hooks/login.ts";
 import Loading from "@pages/@common/common/Loading.tsx";
+import {toast} from "react-toastify";
 
 interface AuthRouteProps {
   element: React.ReactNode;
@@ -17,6 +18,7 @@ const AuthRoute = ({ element, isPrivate }: AuthRouteProps) => {
   }
 
   if (isPrivate && !isLoggedIn) {
+    toast.error("로그인이 필요합니다.")
     return <Navigate to={`${URL_PATH.login}`} replace />;
 
   }
