7e54a11d12b10641f092b62269f234c7c3e61247
carsago <ohseo8813@gmail.com>
2025-03-10 11:21:49 +0900

    refactor: 로그인 상태 갱신하도록 수정

--- src/hooks/@common/useHandleError.ts
+++ src/hooks/@common/useHandleError.ts
@@ -2,14 +2,17 @@ import {useNavigate} from "react-router-dom";
 import {toast} from "react-toastify";
 import {URL_PATH} from "@/constant";
 import {ApiError} from "@/types/apis/response.ts";
+import {useLoginContext} from "@/hooks/login.ts";
 
 export const useHandleError = () => {
   const navigate = useNavigate();
+  const {setIsLoggedInAtom} = useLoginContext()
 
   return (error: Error) => {
     if (!(error instanceof ApiError)) throw error;
     const status = error.status
     if (status === 401) {
+      setIsLoggedInAtom(false)
       toast.error("로그인이 필요합니다.");
       navigate(URL_PATH.login);
     }
--- src/hooks/login.ts
+++ src/hooks/login.ts
@@ -2,9 +2,11 @@ import {useMutation} from '@tanstack/react-query';
 import {getLoginPage, getLoginResult, LoginRequest,} from "@apis/auth.ts";
 import {URL_PATH} from "@/constant";
 import {useNavigate} from "react-router-dom";
-import {useCallback} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {toast} from "react-toastify";
-import {useMemberDetail} from "@/hooks/useMember.ts";
+import {useAtom} from "jotai";
+import {isLoggedInAtom} from "@/contexts/auth/isLoggedInAtom.ts";
+import {getMemberDetail} from "@apis/member.ts";
 
 export const useAuthLoginPage = () => {
   const loginMutation = useMutation({
@@ -31,15 +33,18 @@ export const useAuthLoginPage = () => {
 export const useLogin = (socialProvider: string, code: string) => {
   const redirectUri = getRedirectUri(socialProvider);
   const navigate = useNavigate();
+  const {setIsLoggedInAtom} = useLoginContext()
 
-  const {mutateAsync : mutation} = useMutation({
+  const {mutateAsync: mutation} = useMutation({
     mutationFn: (loginRequest: LoginRequest) =>
         getLoginResult(socialProvider, loginRequest),
     onSuccess: () => {
       navigate(URL_PATH.main);
+      setIsLoggedInAtom(true)
     },
     onError: () => {
       navigate(URL_PATH.login);
+      setIsLoggedInAtom(false)
       toast.error("잠시 후 다시 시도해주세요.")
     },
   });
@@ -52,18 +57,55 @@ export const useLogin = (socialProvider: string, code: string) => {
     mutation(loginRequest)
   }, [mutation, redirectUri, code]);
 
-  return { login };
+  return {login};
 };
+
+export const useLoginContext = () => {
+  const [isLoggedIn, setIsLoggedInAtom] = useAtom(isLoggedInAtom);
+
+  return {
+    isLoggedIn,
+    setIsLoggedInAtom
+  }
+}
+
 
 export const useCheckLogin = () => {
-  const { data, isLoading } = useMemberDetail();
+  const {isLoggedIn, setIsLoggedInAtom} = useLoginContext();
+  const [isLoading, setIsLoading] = useState(true);
 
-  if (isLoading) {
-    return { isLoggedIn: false, isLoading: true };
+  useEffect(() => {
+    const checkLogin = async () => {
+      try {
+        setIsLoading(true);
+
+        if (isLoggedIn === undefined) {
+          const data = await getMemberDetail();
+          setIsLoggedInAtom(!!data);
+        }
+      } catch (error) {
+        console.error("Failed to fetch member details:", error);
+        setIsLoggedInAtom(false);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkLogin();
+  }, [isLoggedIn]);
+
+  if (isLoggedIn) {
+    return {isLoggedIn: isLoggedIn, isLoading: false};
+
   }
 
-  return { isLoggedIn: !!data, isLoading: false };
+  if (isLoading) {
+    return {isLoggedIn: false, isLoading: true};
+  }
+
+  return {isLoggedIn: isLoggedIn, isLoading: false};
 };
+
 function getRedirectUri(socialProvider: string) {
   return `${window.location.origin}${URL_PATH.login}/${socialProvider}`;
 }
