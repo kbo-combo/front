39091f23adb16591aa34e2fd8b13975003bc0a32
carsago <ohseo8813@gmail.com>
2025-03-18 00:13:30 +0900

    feat: Lazy Loading 추가

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,63 +1,80 @@
 import {createBrowserRouter} from 'react-router-dom';
 import {URL_PATH} from "constant";
-import Home from "@pages/@common/home";
+import {lazy, Suspense} from "react";
 import RootTemplate from "@pages/@common/root";
-import Login from "@pages/@common/auth";
-import CallbackPage from "@components/auth/LoginCallback.tsx";
 import AuthRoute from "@/router/AuthRoute.tsx";
-import NotFoundPage from "@pages/@common/common/NotFound.tsx";
-import HitterSelectPage from "@pages/@common/hitter-select";
-import RuleBook from "@pages/@common/rule-book";
-import ComboPage from "@pages/@common/combo/ComboListPage.tsx";
-import MemberPage from "@pages/@common/member/MemberPage.tsx";
-import MemberEditPage from "@pages/@common/member/MemberEditPage.tsx";
-import ComboRankPage from "@pages/@common/rank/ComboRankPage.tsx";
+import Loading from "@pages/@common/common/Loading.tsx";
+
+const Home = lazy(() => import('@pages/@common/home'));
+const Login = lazy(() => import('@pages/@common/auth'));
+const CallbackPage = lazy(() => import('@components/auth/LoginCallback.tsx'));
+const HitterSelectPage = lazy(() => import('@pages/@common/hitter-select'));
+const RuleBook = lazy(() => import('@pages/@common/rule-book'));
+const ComboPage = lazy(() => import('@pages/@common/combo/ComboListPage.tsx'));
+const MemberPage = lazy(() => import('@pages/@common/member/MemberPage.tsx'));
+const MemberEditPage = lazy(() => import('@pages/@common/member/MemberEditPage.tsx'));
+const ComboRankPage = lazy(() => import('@pages/@common/rank/ComboRankPage.tsx'));
+const NotFoundPage = lazy(() => import('@pages/@common/common/NotFound.tsx'));
+
+
+const withSuspense = (component: React.ReactNode) => (
+    <Suspense fallback={<Loading/>}>{component}</Suspense>
+);
 
 const router = createBrowserRouter([
-    {
-      path: URL_PATH.main,
-      element: <RootTemplate/>,
-      errorElement: <NotFoundPage />,
-      children: [
-        {
-          index: true,
-          element: <Home/>,
-        },
-        {
-          path: URL_PATH.hitter_select,
-          element: <HitterSelectPage/>
-        },
-        {
-          path: URL_PATH.login,
-          element: <AuthRoute isPrivate={false} element={<Login />}/>,
-        },
-        {
-          path: `${URL_PATH.login}/:socialProvider`,
-          element: <AuthRoute isPrivate={false} element={<CallbackPage />} />,
-        },
-        {
-          path: URL_PATH.rule_book,
-          element: <RuleBook />,
-        },
-        {
-          path: URL_PATH.rank,
-          element: <ComboRankPage />,
-        },
-        {
-          path: URL_PATH.combo,
-          element: <AuthRoute isPrivate={true} element={<ComboPage />} />,
-        },
-        {
-          path: URL_PATH.member,
-          element: <AuthRoute isPrivate={true} element={<MemberPage />} />,
-        },
-        {
-          path: `${URL_PATH.member}/edit`,
-          element: <AuthRoute isPrivate={true} element={<MemberEditPage />} />,
-        }
-      ],
-    },
-  ]);
+  {
+    path: URL_PATH.main,
+    element: <RootTemplate/>,
+    errorElement: withSuspense(<NotFoundPage/>),
+    children: [
+      {
+        index: true,
+        element: withSuspense(<Home/>),
+      },
+      {
+        path: URL_PATH.hitter_select,
+        element: withSuspense(<HitterSelectPage/>),
+      },
+      {
+        path: URL_PATH.login,
+        element: withSuspense(
+            <AuthRoute isPrivate={false} element={<Login/>}/>
+        ),
+      },
+      {
+        path: `${URL_PATH.login}/:socialProvider`,
+        element: withSuspense(
+            <AuthRoute isPrivate={false} element={<CallbackPage/>}/>
+        ),
+      },
+      {
+        path: URL_PATH.rule_book,
+        element: withSuspense(<RuleBook/>),
+      },
+      {
+        path: URL_PATH.rank,
+        element: withSuspense(<ComboRankPage/>),
+      },
+      {
+        path: URL_PATH.combo,
+        element: withSuspense(
+            <AuthRoute isPrivate={true} element={<ComboPage/>}/>
+        ),
+      },
+      {
+        path: URL_PATH.member,
+        element: withSuspense(
+            <AuthRoute isPrivate={true} element={<MemberPage/>}/>
+        ),
+      },
+      {
+        path: `${URL_PATH.member}/edit`,
+        element: withSuspense(
+            <AuthRoute isPrivate={true} element={<MemberEditPage/>}/>
+        ),
+      },
+    ],
+  },
+]);
 
-
-  export default router;
+export default router;
