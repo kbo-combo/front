a9ebd1b89888c66c5e81b29f56afad1785dade17
carsago <ohseo8813@gmail.com>
2025-03-18 01:04:38 +0900

    refactor: SelectedCombo.tsx 패키지 변경

--- src/pages/@common/home/index.tsx
+++ src/pages/@common/home/index.tsx
@@ -1,10 +1,10 @@
 import GameList from "@components/game/GameList.tsx";
 import GameSchedule from "@components/game/GameSchedule.tsx";
-import SelectedCombo from "@components/combo/SelectedCombo.tsx";
 import {useCheckLogin} from "@/hooks/login.ts";
 import ContentHeader from "@components/@common/contentHeader/ContentHeader.tsx";
 import {PageWrapper} from "@components/@common/wrapper/pageWrapper.style.ts";
 import {useState} from "react";
+import SelectedCombo from "@components/combo/selected/SelectedCombo.tsx";
 
 
 const Home = () => {
